8d692a45dcb599fc1b31f4f3e639b97fe59093cc
iZerus <pavelmi@list.ru>
2024-08-20 15:11:42 +0500

    feat: Версия 1.0

---
+++ src/Log.php
@@ -0,0 +1,209 @@
+<?php
+declare(strict_types=1);
+
+namespace iZerus;
+
+use Error;
+
+/**
+ * Статический логгер с ротацией
+ * @author iZerus
+ * @version 1.0
+ */
+final class Log
+{
+    const A_DEBUG = 2;
+    const A_INFO = 4;
+    const A_WARNING = 8;
+    const A_ERROR = 16;
+    const A_ALL = 32767;
+    private static int $logReportingLevel = self::A_ALL;
+    private static bool $displayLogs = false;
+    private static string $defaultName = "Application";
+    private static bool $initialized = false;
+    private static array $timers;
+    private static array $timings;
+
+    public static function setup(string $path, int $maxSizeForRotate = 10_000_000, int $maxRotatedFilesCount = 10): void
+    {
+        list($major, $minor) = explode(".", phpversion());
+        $version = "$major.$minor";
+        $supportedVersions = ["7.4"];
+        if (!in_array($version, $supportedVersions)) {
+            throw new Error(sprintf("%s не поддерживает версию PHP %s", __CLASS__, $version));
+        }
+        if (!file_exists($path) && (@file_put_contents($path, '') === false)) {
+            throw new Error(sprintf("Не удается создать файл лога по пути %s", $path));
+        }
+        if ($maxSizeForRotate < 1) {
+            throw new Error("Значение 'maxSizeForRotate' не может быть меньше или равно нулю");
+        }
+        if ($maxRotatedFilesCount < 1) {
+            throw new Error("Значение 'maxRotatedFilesCount' не может быть меньше или равно нулю");
+        }
+        ini_set("error_log", $path);
+        ini_set("log_errors", "1");
+        self::$initialized = true;
+        self::rotate($path, $maxSizeForRotate, $maxRotatedFilesCount);
+    }
+
+    public static function startTimer(string $key): void
+    {
+        if (isset(self::$timers[$key])) {
+            throw new Error("Таймер с ключом '$key' уже задан");
+        }
+        self::$timers[$key] = time();
+    }
+
+    public static function stopTimer(string $key): void
+    {
+        if (!isset(self::$timers[$key])) {
+            throw new Error("Таймер с ключом '$key' не задан");
+        }
+        $timing = self::$timers[$key];
+        unset(self::$timers[$key]);
+        self::$timings[$key] = time() - $timing;
+    }
+
+    public static function getTime(string $key): int
+    {
+        if (!isset(self::$timings[$key])) {
+            throw new Error("Тайминг с ключом '$key' не найден или не завершён методом stopTimer()");
+        }
+        return self::$timings[$key];
+    }
+
+    private static function rotate(string $path, int $maxSizeForRotate, int $maxRotatedFilesCount): void
+    {
+        clearstatcache();
+        if (!file_exists($path)) {
+            return;
+        }
+        if (filesize($path) < $maxSizeForRotate) {
+            return;
+        }
+        $directory = dirname($path);
+        $logName = basename($path);
+        // Сохраним в массив все ротированные файлы логов
+        $rotatedFileNames = [];
+        $fileNames = scandir($directory);
+        foreach ($fileNames as $fileName) {
+            if (!preg_match(sprintf("/%s.\d+/", $logName), $fileName)) {
+                continue;
+            }
+            $index = str_replace($logName . '.', '', $fileName);
+            if (!is_numeric($index)) {
+                continue;
+            }
+            $rotatedFileNames[$index] = $fileName;
+        }
+        // Если ротированные файлы есть, то переименуем их, увеличив индекс
+        if (!empty($rotatedFileNames)) {
+            krsort($rotatedFileNames);
+            foreach ($rotatedFileNames as $index => $rotatedFileName) {
+                $newIndex = $index + 1;
+                // Если новый индекс больше максимального количества файлов, то удалим файл
+                if ($newIndex > $maxRotatedFilesCount) {
+                    unlink($directory . '/' . $rotatedFileName);
+                } else {
+                    if (!rename($directory . '/' . $rotatedFileName, $path . "." . $newIndex)) {
+                        throw new Error('Не удалось увеличить (переименовать) индекс ротированного лога');
+                    }
+                }
+            }
+        }
+        // Ротируем текущий файл лога
+        if (!rename($path, $directory . '/' . $logName . "." . 1)) {
+            throw new Error('Не удалось ротировать текущий лог');
+        }
+        if (@file_put_contents($path, '') === false) {
+            throw new Error(sprintf("Не удается создать файл лога по пути %s", $path));
+        }
+    }
+
+    public static function setDefaultName(string $name): void
+    {
+        self::$defaultName = $name;
+    }
+
+    /**
+     * @param int $level константа семейства уровня логов данного класса.
+     * Значение при инициализации Log::A_ALL
+     */
+    public static function setLogReportingLevel(int $level): void
+    {
+        self::$logReportingLevel = $level;
+    }
+
+    /**
+     * @param int $level константа семейства уровня ошибок PHP
+     * @link https://www.php.net/manual/ru/errorfunc.constants.php
+     */
+    public static function setPhpReportingLevel(int $level): void
+    {
+        error_reporting($level);
+    }
+
+    public static function debug(string $message, string $name = null, $data = null): void
+    {
+        self::log(self::A_DEBUG, $message, $name, $data);
+    }
+
+    private static function log(int $level, string $message, string $name = null, $data = null): void
+    {
+        if (!self::$initialized) {
+            throw new Error(__CLASS__ . " не инициализирован. Используйте метод setup");
+        }
+        if (!($level & self::$logReportingLevel)) {
+            return;
+        }
+        $levelNames = [
+            self::A_DEBUG => "Debug",
+            self::A_INFO => "Info",
+            self::A_WARNING => "Warning",
+            self::A_ERROR => "Error"
+        ];
+        if (!key_exists($level, $levelNames)) {
+            throw new Error("Неизвестный уровень логирования");
+        }
+        if ($data) {
+            $data = PHP_EOL . "Data: " . self::varDump($data);
+        }
+        $name = !empty($name) ? $name : self::$defaultName;
+        $text = "$name $levelNames[$level]: " . $message . $data;
+        error_log($text);
+        if (self::$displayLogs) {
+            print $text . PHP_EOL;
+        }
+    }
+
+    private static function varDump($value): string
+    {
+        ob_start();
+        var_dump($value);
+        $content = trim(ob_get_contents());
+        ob_end_clean();
+        return $content;
+    }
+
+    public static function info(string $message, string $name = null, $data = null): void
+    {
+        self::log(self::A_INFO, $message, $name, $data);
+    }
+
+    public static function warning(string $message, string $name = null, $data = null): void
+    {
+        self::log(self::A_WARNING, $message, $name, $data);
+    }
+
+    public static function error(string $message, string $name = null, $data = null): void
+    {
+        self::log(self::A_ERROR, $message, $name, $data);
+    }
+
+    public static function setDisplayLogs(bool $displayLogs): void
+    {
+        self::$displayLogs = $displayLogs;
+        ini_set("display_errors", $displayLogs ? "on" : "off");
+    }
+}
---
+++ tests/log-test.php
@@ -0,0 +1,419 @@
+<?php
+declare(strict_types=1);
+
+use iZerus\Log;
+
+chdir(__DIR__);
+
+require_once "../src/Log.php";
+
+const LOG_PATH = "../logs/log-test.log";
+const LOG_ROTATE_PATH = "../logs/log-test-rotate.log";
+
+function printFailedTest(string $testName, ?string $expected = null, ?string $current = null) {
+    echo "$testName провален" . PHP_EOL;
+    if ($expected !== null) {
+        echo "Ожидаемо: " . $expected . PHP_EOL;
+    }
+    if ($current !== null) {
+        echo "Результат: " . $current . PHP_EOL;
+    }
+    exit;
+}
+
+function readLastLine(string $filename, int $offset = 0): string
+{
+    $data = file($filename);
+    if ($data === false) {
+        throw new Error('Не удалось прочитать файл лога');
+    }
+    return trim($data[count($data) - 1 - $offset]);
+}
+
+function matchLogLine(string $pattern, string $logLine, bool $matchDate = true)
+{
+    if ($matchDate && !preg_match("/^\[\d+-\S+-\d+ \d+:\d+:\d+\s\S+] /", $logLine, $matches)) {
+        return false;
+    }
+    return preg_match($pattern, !empty($matches) ? str_replace($matches[0], '', $logLine) : $logLine);
+}
+
+/*
+ * Инициализация лога
+ */
+// Тест использования без инициализации
+{
+    $caught = false;
+    try {
+        Log::info("Тестовое сообщение");
+    } catch (Error $e) {
+        if (preg_match("/не инициализирован. Используйте метод setup/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на инициализацию",
+            "Будет брошено исключение",
+            "Не брошено"
+        );
+    }
+}
+
+/*
+ * Конфигурации файла лога
+ */
+// Тест на невозможность создать файл лога
+{
+    $caught = false;
+    $failedPath = __DIR__ . "../logs/failed.log";
+    try {
+        LOG::setup($failedPath);
+    } catch (Error $e) {
+        if (preg_match("/^Не удается создать файл лога по пути/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на невозможность создать файл лога",
+            "Будет брошено исключение",
+            "Не брошено"
+        );
+    }
+
+}
+// Тест установки директивы файла логов
+{
+    $tmpValue = "tmp" . time();
+    $previousValue = ini_set("error_log", $tmpValue);
+    Log::setup(LOG_PATH);
+    if (basename(ini_get("error_log")) !== basename(LOG_PATH)) {
+        printFailedTest("Тест установки директивы файла логов",
+            LOG_PATH,
+            basename(ini_get("error_log"))
+        );
+    }
+}
+// Тест установки директивы записи ошибок в лог
+{
+    ini_set("log_errors", "0");
+    Log::setup(LOG_PATH);
+    if (ini_get("log_errors") != "1") {
+        printFailedTest("Тест установки директивы записи ошибок в лог",
+            "1",
+            ini_get("log_errors")
+        );
+    }
+}
+Log::setup(LOG_PATH);
+// Тест установки размера файла для условия ротации лога
+{
+    $caught = false;
+    try {
+        Log::setup(LOG_PATH, 0);
+    } catch (Error $e) {
+        if (preg_match("/^Значение 'maxSizeForRotate' не может быть меньше или равно нулю/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на неадекватный параметр 'maxSizeForRotate'",
+            "Будет брошено исключение",
+            "Исключение не брошено"
+        );
+    }
+}
+// Тест установки размера файла для условия ротации лога
+{
+    $caught = false;
+    try {
+        Log::setup(LOG_PATH, 5, 0);
+    } catch (Error $e) {
+        if (preg_match("/^Значение 'maxRotatedFilesCount' не может быть меньше или равно нулю/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на неадекватный параметр 'maxRotatedFilesCount'",
+            "Будет брошено исключение",
+            "Исключение не брошено"
+        );
+    }
+}
+
+/*
+ * Прочие конфигурации
+ */
+// Тест установки директивы вывода ошибок
+{
+    ini_set("display_errors", "off");
+    Log::setDisplayLogs(true);
+    if (ini_get("display_errors") === "off") {
+        printFailedTest("Тест установки директивы вывода ошибок",
+            "on",
+            ini_get("display_errors")
+        );
+    }
+    ini_set("display_errors", "on");
+}
+Log::setDisplayLogs(false);
+// Тест установки директивы уровня вывода ошибок PHP
+{
+    error_reporting(E_ALL);
+    Log::setPhpReportingLevel(E_NOTICE);
+    if ((int)ini_get("error_reporting") !== E_NOTICE) {
+        printFailedTest("Тест установки директивы вывода ошибок",
+            (string)E_NOTICE,
+            ini_get("error_reporting")
+        );
+    }
+}
+error_reporting(E_ALL);
+
+/*
+ * Вывод логов
+ */
+// Тест на вывод Debug
+{
+    Log::debug("Debug message");
+    if (!matchLogLine("/^Application Debug: Debug message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'Application Debug'",
+            "[Дата] Application Debug: Debug message",
+            readLastLine(LOG_PATH));
+    }
+}
+// Тест на вывод Info
+{
+    Log::info("Info message");
+    if (!matchLogLine("/^Application Info: Info message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'Application Info'",
+            "[Дата] Application Info: Info message",
+            readLastLine(LOG_PATH));
+    }
+}
+// Тест на вывод Warning
+{
+    Log::warning("Warning message");
+    if (!matchLogLine("/^Application Warning: Warning message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'Application Warning'",
+            "[Дата] Application Warning: Warning message",
+            readLastLine(LOG_PATH));
+    }
+}
+// Тест на вывод Error
+{
+    Log::error("Error message");
+    if (!matchLogLine("/^Application Error: Error message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'Application Error'",
+            "[Дата] Application Error: Error message",
+            readLastLine(LOG_PATH));
+    }
+}
+
+/*
+ * Вывод с данными
+ */
+// Тест на вывод с данными
+{
+    Log::error("Error message", null, 128);
+    if (!matchLogLine("/^Data: int\(128\)$/", readLastLine(LOG_PATH), false)) {
+        printFailedTest("Тест на вывод с данными",
+            "Data: int(128)",
+            readLastLine(LOG_PATH));
+    }
+}
+
+/*
+ * Уровни логирования
+ */
+// Тест на уровень Info
+{
+    Log::setLogReportingLevel(Log::A_INFO);
+    Log::info("Info message");
+    Log::warning("Warning message");
+    Log::error("Error message");
+    if (!matchLogLine("/^Application Info: Info message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на уровень Info",
+            "[Дата] Application Info: Info message",
+            readLastLine(LOG_PATH));
+    }
+}
+// Тест на уровень Info и Error
+{
+    Log::setLogReportingLevel(Log::A_INFO | Log::A_ERROR);
+    Log::info("Info message");
+    Log::warning("Warning message");
+    Log::error("Error message");
+    if (!matchLogLine("/^Application Info: Info message/", readLastLine(LOG_PATH, 1))) {
+        printFailedTest("Тест на уровень Info и Error",
+            "[Дата] Application Info: Info message",
+            readLastLine(LOG_PATH));
+    }
+    if (!matchLogLine("/^Application Error: Error message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на уровень Info и Error",
+            "[Дата] Application Error: Error message",
+            readLastLine(LOG_PATH));
+    }
+}
+
+/*
+ * Именованный вывод логов
+ */
+// Вывод лога от имени TestApp
+{
+    Log::info("Info message", "TestApp");
+    if (!matchLogLine("/^TestApp Info: Info message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на именованный лог 'TestApp Info'",
+            "[Дата] TestApp Info: Info message",
+            readLastLine(LOG_PATH));
+    }
+}
+// Вывод лога от имени App
+{
+    Log::setDefaultName("App");
+    Log::info("Info message");
+    if (!matchLogLine("/^App Info: Info message/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на именованный дефолтный лог 'App Info'",
+            "[Дата] App Info: Info message",
+            readLastLine(LOG_PATH));
+    }
+}
+Log::setDefaultName("Application");
+
+/*
+ * Тест таймеров
+ */
+// Тест на корректность работы таймера
+{
+    Log::startTimer('test');
+    sleep(1);
+    Log::stopTimer('test');
+    $time = Log::getTime('test');
+    if ($time !== 1) {
+        printFailedTest("Тест на таймер", '1', "$time");
+    }
+}
+// Тест на неверный ключ завершения таймера
+{
+
+    $caught = false;
+    try {
+        Log::stopTimer('stop');
+    } catch (Error $e) {
+        if (preg_match("/^Таймер с ключом 'stop' не задан/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на неверный ключ завершения таймера",
+            "Будет брошено исключение",
+            "Исключение не брошено"
+        );
+    }
+}
+// Тест на неверный ключ получения времени
+{
+
+    $caught = false;
+    try {
+        Log::getTime('time');
+    } catch (Error $e) {
+        if (preg_match("/^Тайминг с ключом 'time' не найден или не завершён методом stopTimer/", $e->getMessage())) {
+            $caught = true;
+        } else {
+            throw $e;
+        }
+    }
+    if (!$caught) {
+        printFailedTest("Тест на неверный ключ получения времени",
+            "Будет брошено исключение",
+            "Исключение не брошено"
+        );
+    }
+}
+
+/*
+ * Тест ротации
+ */
+{
+    for ($j = 1; $j <= 10; $j++) {
+        Log::setup(LOG_ROTATE_PATH, 1, 5);
+        for ($i = 1; $i <= 10; $i++) {
+            Log::info("Ротация $j#$i");
+        }
+    }
+    if (!matchLogLine("/^Application Info: Ротация 10#10/", readLastLine(LOG_ROTATE_PATH))) {
+        printFailedTest("Тест ротации логов",
+            "[Дата] Application Info: Ротация 10#10",
+            readLastLine(LOG_ROTATE_PATH));
+    }
+    $fileNames = scandir(dirname(LOG_ROTATE_PATH));
+    $successRotatedLogs = 0;
+    foreach ($fileNames as $fileName) {
+        if (!preg_match(sprintf("/%s.\d+/", basename(LOG_ROTATE_PATH)), $fileName)) {
+            continue;
+        }
+        $index = str_replace(basename(LOG_ROTATE_PATH) . '.', '', $fileName);
+        if (!is_numeric($index)) {
+            continue;
+        }
+        $value = 10 - $index;
+        if (!matchLogLine("/^Application Info: Ротация $value#10/", readLastLine(dirname(LOG_ROTATE_PATH) . '/' . $fileName))) {
+            printFailedTest("Тест ротации логов",
+                "[Дата] Application Info: Ротация $value#10",
+                readLastLine(LOG_ROTATE_PATH));
+        }
+        $successRotatedLogs++;
+    }
+    if ($successRotatedLogs !== 5) {
+        printFailedTest("Тест ротации логов",
+            "5 ротаций лога",
+            $successRotatedLogs . " ротаций лога");
+    }
+}
+
+/*
+ * Вывод ошибок PHP
+ */
+Log::setup(LOG_PATH);
+// Тест на лог 'PHP Notice'
+{
+    $arr = [1, 2, 3];
+    echo $arr[10]; // Вызывает E_NOTICE
+    if (!matchLogLine("/^PHP Notice: .*/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'PHP Notice'",
+            "[Дата] PHP Notice: Уведомление",
+            readLastLine(LOG_PATH));
+    }
+}
+// Тест на лог 'PHP Warning'
+{
+    filemtime("testFile" . time()); // Вызывает E_WARNING
+    if (!matchLogLine("/^PHP Warning: .*/", readLastLine(LOG_PATH))) {
+        printFailedTest("Тест на лог 'PHP Warning'",
+            "[Дата] PHP Warning: Предупреждение",
+            readLastLine(LOG_PATH));
+    }
+}
+// Завершающий тест на лог 'PHP Error'
+{
+    register_shutdown_function(function () {
+        if (!matchLogLine("/^PHP Fatal error:\s\s.*/", readLastLine(LOG_PATH, 3))) {
+            printFailedTest("Тест на лог 'PHP Fatal error'",
+                "[Дата] PHP Fatal error:\s\s.*",
+                readLastLine(LOG_PATH));
+        }
+        print "Тесты пройдены успешно";
+    });
+    $funName = "testFunction";
+    $funName(); // Вызывает E_ERROR
+}
